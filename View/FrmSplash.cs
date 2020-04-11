50ecd7a3bb3b6b84c7ec81f31d0965a9bd628ba7
GabrielDevCSharp <gabriel0211oliveira@outlook.com>
2020-04-11 17:21:13 -0300

    projeto techShopping

---
+++ View/FrmSplash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class FrmSplash : Form
+    {
+        public FrmSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcessar_Click(object sender, EventArgs e)
+        {
+            FrmHome frmHome = new FrmHome();
+            frmHome.Show();
+            this.Hide();
+        }
+
+        private void btnConfiguracoes_Click(object sender, EventArgs e)
+        {
+            FrmConfiguracoes frmConfiguracoes = new FrmConfiguracoes();
+            frmConfiguracoes.ShowDialog();
+        }
+    }
+}
